bb453647de82863cecadc4c78e54621bfce5d45a
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-04-05 01:13:48 +0530

    cyclic sort

---
+++ Sorting/Cyclic_sort.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+public class Cyclic_sort {
+
+    static void cyclic(int [] arr){
+        for(int i=0;i<arr.length;){
+            if(arr[i]==i+1){
+                i++;
+            }
+            else{
+                swap(arr,i,arr[i]-1);
+            }
+        }
+    }
+
+    static void swap(int [] arr, int a,int b){
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+    public static void main(String[] args) {
+        int [] arr={5,4,3,2,1};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
