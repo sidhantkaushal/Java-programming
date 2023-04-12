6aa091072a6a17447597d373461696c712cb159e
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-04-12 23:19:47 +0530

    Did questions using cyclic sort.

--- Sorting/Cyclic_sort.java
+++ Sorting/Cyclic_sort.java
@@ -3,6 +3,19 @@ public class Cyclic_sort {
 
     static void cyclic(int [] arr){
         for(int i=0;i<arr.length;){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    
+    static void cyclic2(int [] arr){
+        int i=0;
+        while(i<arr.length){
             if(arr[i]==i+1){
                 i++;
             }
@@ -18,7 +31,7 @@ public class Cyclic_sort {
         arr[b]=temp;
     }
     public static void main(String[] args) {
-        int [] arr={5,4,3,2,1};
+        int [] arr={5,4,3,2,1,2,3,};
         cyclic(arr);
         System.out.println(Arrays.toString(arr));
     }
