c7c040e0a8497a1afb4f175aa0cd02816c10c99f
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-04-14 22:55:37 +0530

    Search in duplicates sorted arrays

--- Searching/Binary Search/SearchRotated.java
+++ Searching/Binary Search/SearchRotated.java
@@ -2,9 +2,9 @@ public class SearchRotated
 { 
     // Search in a rotated sorted array. 
     public static void main(String[] args) {
-        int [] arr={4,5,6,7,8,4,3,2,1};
-        System.out.println(find_pivot(arr));
-        System.out.println(searchInRotated(arr,6));
+        int [] arr={4,5,6,7,7,8,9,4,3,3,3,2,1};
+        System.out.println(find_pivotWithDuplicates(arr));
+        System.out.println(searchInRotated(arr,3));
         // System.out.println(searchInRotated(arr));
     }
 
@@ -62,6 +62,41 @@ public class SearchRotated
          }
          return -1;
     }
+    static int find_pivotWithDuplicates(int [] arr)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end)
+        {
+           
+       int mid = start + (end-start)/2;
+           if( mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+           }
+           if( mid>start && arr[mid]<arr[mid-1]){
+               return mid-1;
+           }
+           if(arr[start]==arr[mid] && arr[end]==arr[mid]){
+            if(arr[start]>arr[start+1]){
+                return start;
+            }
+            start++;
+            if(arr[end]<arr[end-1])
+            {
+                return end-1;
+            }
+            end--;
+           }
+           else if (arr[start]==arr[mid] || (arr[start]==arr[mid]&&arr[mid]>arr[end])){
+                start=mid+1;
+           }
+           else{
+            end=mid-1;
+           }
+
+        }
+        return -1;
+   }
 
 
 
